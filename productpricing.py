dc700ccdd3f0e1a7642bd0919c3aeed6c171d4dd
alfonsojmendoza <alfonsojmendoza@users.noreply.github.com>
2015-09-10 22:55:11 -0700

    Update and rename roompricing.py to productpricing.py

--- productpricing.py
+++ productpricing.py
@@ -1,4 +1,5 @@
 """
+
 PRICING OF NEW INVENTORY
 
 PURPOSE:
@@ -22,72 +23,70 @@ METHOD:
 """
 
 import csv as csv
-from itertools import permutations
 import numpy as np
-import tkinter as tk 
-
+#import tkinter as tk 
 
 onlineform = []
-listroomnames = []
-baseprice = []
 
 #  House details needed from Salesforce, change for each new house
-house_rent = float(7250)
+house_rent = float(7500)
 house_sqft = float(1900)
 house_upmark = float(150)
-membershipprice = (150)
+membershipprice = float(150)
 
 # Affects range of room prices, typically '0.03'
 # 	Check & Balance for Marketing / Sales team  
 weightpercent = float(0.03)
 
-
 #Ops Team submits file with following info [Room Name][Room Sq Ft][Var 1][Var 2][Var 3]
-def preponlineformdata(readsomedata):
-	readdata = csv.reader(open("elsie.csv","r"))   #name of property with info	
-	for row in readdata:
-		onlineform.append(row)
-	header_online = onlineform[0]
-	onlineform.pop(0)
-	return (onlineform)
-
-def removeroomnames(listwithstring):
-	for j, y in enumerate(listwithstring):
-		aroomname = y[0]
-		listroomnames.append(aroomname)
-		y.pop(0)
-
-#--------------------------------------------------------#
-# basepricecalculation ==
-#		Main algorithm for calculating price of each room
-#
-#--------------------------------------------------------#
-
-def basepricecalculation(roomdata):
-	for x in roomdata:
-		roombaseprice = (house_rent / house_sqft ) * \
-			(((house_sqft - interiorspace)/roomcount) + x[0] )
-		baseprice.append(roombaseprice)
+class ImportCSVdata(object):
+	def __init__(self, filename, dataframe):
+		self.filename = filename
+		self.dataframe = dataframe
+		self.makedataframe()
+		
+	def makedataframe(self):
+		readdata = csv.reader(open(self.filename,"r"))
+		for row in readdata:
+			self.dataframe.append(row)
+		self.dataframe.pop(0)			#remover header, set to variable if needed
+		return (self.dataframe)
 
 
 # Prepping data from ops team
-preponlineformdata(onlineform)
-RemoveRoomNames(onlineform)
+userinput = []
+ImportCSVdata("elsie.csv", userinput)
 
-# Prepping variables for algorithm
-roomcount = len(listroomnames)
-roomdata = np.array(onlineform, dtype = float)	
+
+#list of just roomnames
+roomnames = list(x[0] for x in userinput)
+
+#list withoutroomnames, used for calculations
+roommeasurements = list(x[1:] for x in userinput)
+
+
+#--------------------------------------------------------#
+# baseprice ==
+#		Main algorithm for calculating price of each room
+#
+#--------------------------------------------------------#
+#variables used in algorithm
+roomcount = len(roomnames)
+roomdata = np.array(roommeasurements, dtype = float)	
 roomvariable = roomdata[:,1:4].sum(axis = 1)	# Sum of Room Variable Scores
 interiorspace = int(roomdata[:,:1].sum(axis = 0))	 # Sum of Room (sq ft)
 
 
-basepricecalculation(roomdata)
+baseprice = [((house_rent / house_sqft ) * (((house_sqft - interiorspace)/roomcount) + x[0] )) for x in roomdata]
 baseprice = np.array(baseprice)
+
 
 # Normalizing price so sum of rooms == total rent
 sumproductnow = (baseprice * roomvariable).sum(axis = 0)
+
 normalizedprice = (house_rent/(weightpercent*sumproductnow+house_rent)*
-								(weightpercent* baseprice * roomvariable + baseprice))
+				(weightpercent* baseprice * roomvariable + baseprice))
+
 normalizedprice = np.around(normalizedprice,decimals = -1)
 
 
@@ -95,11 +94,7 @@ normalizedprice = np.around(normalizedprice,decimals = -1)
 publishedprice = normalizedprice + membershipprice
 
 print ("\nRoom Name\t\tNormalized ($)\t\tPublished Price ($)")
-for x,y,z in zip(normalizedprice,listroomnames,publishedprice):
-	print ("\t%s	\t\t$  %s \t\t\t$  %s" %(y, x, z))
-
-print ("\nSum of Calcuations:\t$", normalizedprice.sum(axis = 0))
-
-
-#TODO:  v2 - GUI (via Tkinter) 
-#TODO:  v3 - API to get info from Salesforce 
+for x,y,z in zip(normalizedprice,roomnames,publishedprice):
+	#print ("\t%s	\t\t$  %s \t\t\t$  %s" %(y, x, z))
+	print ("\t{}\t\t\t$ {} \t\t\t$ {}".format(y,x,z))
+print ("\nSum of Calcuations:\t$ {} \t Monthly Rent:\t {}".format(normalizedprice.sum(axis = 0), house_rent))
